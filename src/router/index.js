c4c7b9b802853019288a602b59b9835659305062
48n02m41 <54042201+48n02m41@users.noreply.github.com>
2020-03-01 01:10:52 +0000

    Auth0 login working. Not hooked up to API yet.

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import { authGuard } from "../auth/authGuard";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue")
+  },
+  {
+    path: "/event/:id",
+    name: "eventSingle",
+    component: () => import("../views/EventSingle.vue"),
+    beforeEnter: authGuard
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
